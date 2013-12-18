a2cd8dbefc8d06e4ac65008119b8743d0a7e22f2
Matt Teagarden <matt@n0x.net>
2013-12-18 18:25:15 +0000

    Figured out how to add multiple pages

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,4 +1,11 @@
 class PortfolioController < ApplicationController
   def index
+    render "portfolio/index"
   end
+
+  def brands
+    render "portfolio/brands"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Simpleuxdesign::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root :to => "portfolio#index"
-
+  
+  get "/index" => 'portfolio#index'
+  get "/brands" => 'portfolio#brands'
+  
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
